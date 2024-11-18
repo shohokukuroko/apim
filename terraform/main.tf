52d7f8d7d3cd2a6c965cf71cebdb32814e1144ce
shohokukuroko <goku8naruto@gmail.com>
2024-11-19 01:27:35 +0530

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -60,16 +60,3 @@ resource "azurerm_api_management_api" "api" {
   protocols           = ["https"]
   service_url         = azurerm_function_app.function.default_hostname
 }
-
-# Outputs
-output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
-}
-
-output "function_app_url" {
-  value = azurerm_function_app.function.default_hostname
-}
-
-output "apim_url" {
-  value = azurerm_api_management.apim.gateway_url
-}
